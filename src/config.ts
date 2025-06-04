315f59c6587f1cff3f4dd5bb534ef1153f350e79
srajan <you@example.com>
2025-06-04 08:14:22 -0700

    added .evn

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,2 @@
-export const JWT_SECRET = process.env.JWT_SECRET || "your_default_secret";
-export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/yourdb";
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const MONGODB_URI = process.env.MONGODB_URI;
